d248d23b29f41d612582381c09400d7fa15e0fbc
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-01-17 17:11:34 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -60
+})
+let mySprite: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . f f . . . 
     . . f 1 1 f . . 
     . f 1 9 f 9 f . 
@@ -9,3 +13,10 @@ let mySprite = sprites.create(img`
     . . . . . . . . 
     . . f 6 6 f . . 
     `, SpriteKind.Player)
+tiles.loadMap(tiles.createSmallMap(tilemap`level7`))
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile3`)
+controller.moveSprite(mySprite, 50, 0)
+forever(function () {
+    mySprite.vy += 2.5
+})
