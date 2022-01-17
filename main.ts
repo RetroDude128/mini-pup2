0aad93247017fb508e9d3e20ee9a3c3404833578
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2022-01-17 16:55:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . . f f . . . 
+    . . f 1 1 f . . 
+    . f 1 9 f 9 f . 
+    . f 1 9 f 9 f . 
+    . . f 1 1 f . . 
+    . . . f f . . . 
+    . . . . . . . . 
+    . . f 6 6 f . . 
+    `, SpriteKind.Player)
